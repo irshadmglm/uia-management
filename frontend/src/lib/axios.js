2c83e3203b09ece42bb59c924d83019505ec3908
irshad <irshadmglm@gmail.com>
2025-05-21 18:44:58 +0530

    updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ const hostname = window.location.hostname;
 //  "localhost" or "192.168.1.7"
 
 export const axiosInstance = axios.create({
-  baseURL: `http://${hostname}:5000/api`,
+  baseURL: import.meta.env.MODE === "development" ? `http://${hostname}:5000/api` : "/api",
   withCredentials: true,
 });
