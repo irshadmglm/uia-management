e938f99661681f884a391172390fe8ec605f3e4a
irshad <irshadmglm@gmail.com>
2025-05-26 11:55:25 +0530

    ready to deploy

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 
-const hostname = window.location.hostname;  
-//  "localhost" or "192.168.1.7"
+const hostname = window.location.hostname;
+
+const baseURL =
+  import.meta.env.MODE === "development"
+    ? `http://${hostname}:5000/api`
+    : import.meta.env.VITE_BACKEND_URL;
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? `http://${hostname}:5000/api` : "/api",
+  baseURL,
   withCredentials: true,
 });
