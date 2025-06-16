3c7e1efbe855a19bcc1ed4a0b1fe643f617c0b0e
irshad <irshadmglm@gmail.com>
2025-06-16 10:55:19 +0530

    modified

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -20,20 +20,26 @@ export default defineConfig({
           {
             src: '/web-app-manifest-192x192.png',
             sizes: '192x192',
-            type: 'image/png',
+            type: 'image/png'
           },
           {
             src: '/web-app-manifest-512x512.png',
             sizes: '512x512',
-            type: 'image/png',
+            type: 'image/png'
           },
           {
-            src: '/web-app-manifest-512x512.png',
+            src: '/web-app-manifest-192x192-maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/web-app-manifest-512x512-maskable.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable',
+            purpose: 'maskable'
           },
-        ],
+        ]
       },
     }),
   ],
