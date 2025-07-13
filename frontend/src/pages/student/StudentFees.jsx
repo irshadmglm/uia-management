a9abce8d880517a26606f930ff81a90aa3721dad
irshad <irshadmglm@gmail.com>
2025-07-13 23:09:12 +0530

    updated spinning

--- frontend/src/pages/student/StudentFees.jsx
+++ frontend/src/pages/student/StudentFees.jsx
@@ -7,7 +7,7 @@ const months = [ "شوّال", "ذو القعدة", "ذو الحجة", "محرّ
    "ربيع الآخر", "جمادى الأولى", "جمادى الأخرى", "رجب", "شعبان", "رمضان"];
 
 const Spinner = () => (
-  <div className="flex justify-center items-center py-10">
+  <div className="flex justify-center items-center py-10 h-screen">
     <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900 dark:border-gray-100" />
   </div>
 );
